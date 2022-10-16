61a99759cd77f4d459fe59a08030da86abccaf7b
Alexander Germek <alexander.hermek@gmail.com>
2022-10-16 19:50:26 +0600

    first commit

---
+++ BoomerangCardEffect/BoomerangCardEffect/BoomerangCardEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoomerangCardEffectApp.swift
+//  BoomerangCardEffect
+//
+//  Created by Alexander Germek on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct BoomerangCardEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoomerangCardEffect/BoomerangCardEffect/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BoomerangCardEffect
+//
+//  Created by Alexander Germek on 16.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	var body: some View {
+		VStack {
+			Image(systemName: "globe")
+				.imageScale(.large)
+				.foregroundColor(.accentColor)
+			Text("Hello, world!")
+		}
+		.padding()
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
